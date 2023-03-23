47c8454b8ae0cf8a689884093e7fca46e9f956a3
amazingJiyu <87930686+amazingJiyu@users.noreply.github.com>
2023-03-24 08:32:37 +0900

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,51 +1,57 @@
-const quotes = [
-    {
-        quote : "test1",
-        author : "test1_1",
-    },
-    {
-        quote : "test2",
-        author : "test2_1",
-    },
-    {
-        quote : "test3",
-        author : "test3_1",
-    },
-    {
-        quote : "test4",
-        author : "test4_1",
-    },
-    {
-        quote : "test5",
-        author : "test5_1",
-    },
-    {
-        quote : "test6",
-        author : "test6_1",
-    },
-    {
-        quote : "test7",
-        author : "test7_1",
-    },
-    {
-        quote : "test8",
-        author : "test8_1",
-    },
-    {
-        quote : "test9",
-        author : "test9_1",
-    },
-    {
-        quote : "test10",
-        author : "test10_1",
-    }
-]
 
+  const quotes = [
+    {
+      quote: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt",
+    },
+    {
+      quote: "You miss 100% of the shots you don't take.",
+      author: "Wayne Gretzky",
+    },
+    {
+      quote:
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      author: "Winston Churchill",
+    },
+    {
+      quote: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs",
+    },
+    {
+      quote:
+        "Success is walking from failure to failure with no loss of enthusiasm.",
+      author: "Winston Churchill",
+    },
+    {
+      quote:
+        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      author: "Nelson Mandela",
+    },
+    {
+      quote: "Don't let yesterday take up too much of today.",
+      author: "Will Rogers",
+    },
+    {
+      quote:
+        "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+      author: "Christian D. Larson",
+    },
+    {
+      quote:
+        "The future belongs to those who believe in the beauty of their dreams.",
+      author: "Eleanor Roosevelt",
+    },
+    {
+      quote:
+        "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+      author: "Zig Ziglar",
+    },
+  ];
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+  const quote = document.querySelector("#quote span:first-child");
+  const author = document.querySelector("#quote span:last-child");
 
-const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+  quote.innerText = todaysQuote.quote;
+  author.innerText = todaysQuote.author;
